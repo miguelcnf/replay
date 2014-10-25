77118c3707613d6fc3356f2a9ebccc8bd891e435
Miguel Fonseca <miguel.cnf@gmail.com>
2014-10-25 14:05:10 +0100

    Fix identation to four spaces.

--- lib/replay/__init__.py
+++ lib/replay/__init__.py
@@ -2,13 +2,13 @@
 from subprocess import check_call
 
 class replay:
-  ''' replay wrapper '''
+    ''' replay wrapper '''
 
-  def run(self, playbook_file, inventory_file):
-    ''' hacky syscall to run ansible-playbook '''
+    def run(self, playbook_file, inventory_file):
+        ''' hacky syscall to run ansible-playbook '''
 
-    try:
-      run_ansible_playbook = 'ansible-playbook -i %s %s' % (inventory_file, playbook_file)
-      check_call(['/bin/bash', '-c', run_ansible_playbook])
-    except Exception, e:
-      raise Exception('Unexpected: %s at module %s' % (e, self.__class__.__name__))
+        try:
+            run_ansible_playbook = 'ansible-playbook -i %s %s' % (inventory_file, playbook_file)
+            check_call(['/bin/bash', '-c', run_ansible_playbook])
+        except Exception, e:
+            raise Exception('Unexpected: %s at module %s' % (e, self.__class__.__name__))
